90a91c2df72383cf99a5d2bb9838b8099f3b90e4
chizihn <chizihn@gmail.com>
2025-10-17 09:11:57 +0100

    sigh

--- src/graphql/types/product.type.ts
+++ src/graphql/types/product.type.ts
@@ -1,71 +1,71 @@
-import { Field, ID, ObjectType, InputType, registerEnumType, Float } from "type-graphql";
-import { ProductStatus } from "@prisma/client";
-import { User } from "./user.type";
+// import { Field, ID, ObjectType, InputType, registerEnumType, Float } from "type-graphql";
+// import { ProductStatus } from "@prisma/client";
+// import { User } from "./user.type";
 
-registerEnumType(ProductStatus, {
-  name: "ProductStatus",
-  description: "Status of a marketplace product/listing",
-});
+// registerEnumType(ProductStatus, {
+//   name: "ProductStatus",
+//   description: "Status of a marketplace product/listing",
+// });
 
-@ObjectType()
-export class Product {
-  @Field(() => ID)
-  id?: string;
+// @ObjectType()
+// export class Product {
+//   @Field(() => ID)
+//   id?: string;
 
-  @Field(() => User)
-  seller?: User;
+//   @Field(() => User)
+//   seller?: User;
 
-  @Field(() => String)
-  title?: string;
+//   @Field(() => String)
+//   title?: string;
 
-  @Field(() => String)
-  description?: string;
+//   @Field(() => String)
+//   description?: string;
 
-  @Field(() => Float)
-  price?: number;
+//   @Field(() => Float)
+//   price?: number;
 
-  @Field(() => String)
-  imageUrl?: string;
+//   @Field(() => String)
+//   imageUrl?: string;
 
-  @Field(() => ProductStatus)
-  status?: ProductStatus;
+//   @Field(() => ProductStatus)
+//   status?: ProductStatus;
 
-  @Field(() => Date)
-  createdAt?: Date;
+//   @Field(() => Date)
+//   createdAt?: Date;
 
-  @Field(() => Date)
-  updatedAt?: Date;
-}
+//   @Field(() => Date)
+//   updatedAt?: Date;
+// }
 
-@InputType()
-export class CreateProductInput {
-  @Field(() => String)
-  title!: string;
+// @InputType()
+// export class CreateProductInput {
+//   @Field(() => String)
+//   title!: string;
 
-  @Field(() => String)
-  description!: string;
+//   @Field(() => String)
+//   description!: string;
 
-  @Field(() => Float)
-  price!: number;
+//   @Field(() => Float)
+//   price!: number;
 
-  @Field(() => String)
-  imageUrl!: string;
-}
+//   @Field(() => String)
+//   imageUrl!: string;
+// }
 
-@InputType()
-export class UpdateProductInput {
-  @Field(() => String, { nullable: true })
-  title?: string;
+// @InputType()
+// export class UpdateProductInput {
+//   @Field(() => String, { nullable: true })
+//   title?: string;
 
-  @Field(() => String, { nullable: true })
-  description?: string;
+//   @Field(() => String, { nullable: true })
+//   description?: string;
 
-  @Field(() => Float, { nullable: true })
-  price?: number;
+//   @Field(() => Float, { nullable: true })
+//   price?: number;
 
-  @Field(() => String, { nullable: true })
-  imageUrl?: string;
+//   @Field(() => String, { nullable: true })
+//   imageUrl?: string;
 
-  @Field(() => ProductStatus, { nullable: true })
-  status?: ProductStatus;
-} 
+//   @Field(() => ProductStatus, { nullable: true })
+//   status?: ProductStatus;
+// } 
