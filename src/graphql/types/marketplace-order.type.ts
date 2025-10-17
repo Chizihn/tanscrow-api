521f2fcd6fdde837df29605bd7e2bdeb746b7b02
chizihn <chizihn@gmail.com>
2025-10-17 09:10:23 +0100

    removed marketplace causing deploy ish

--- src/graphql/types/marketplace-order.type.ts
+++ src/graphql/types/marketplace-order.type.ts
@@ -1,74 +1,74 @@
-import { Field, ID, ObjectType, InputType, registerEnumType, Float } from "type-graphql";
-import { MarketplaceOrderStatus, EscrowStatus } from "@prisma/client";
-import { Product } from "./product.type";
-import { User } from "./user.type";
+// import { Field, ID, ObjectType, InputType, registerEnumType, Float } from "type-graphql";
+// import { MarketplaceOrderStatus, EscrowStatus } from "@prisma/client";
+// import { Product } from "./product.type";
+// import { User } from "./user.type";
 
-registerEnumType(MarketplaceOrderStatus, {
-  name: "MarketplaceOrderStatus",
-  description: "Status of a marketplace order",
-});
+// registerEnumType(MarketplaceOrderStatus, {
+//   name: "MarketplaceOrderStatus",
+//   description: "Status of a marketplace order",
+// });
 
-registerEnumType(EscrowStatus, {
-  name: "EscrowStatus",
-  description: "Status of escrow for an order",
-});
+// registerEnumType(EscrowStatus, {
+//   name: "EscrowStatus",
+//   description: "Status of escrow for an order",
+// });
 
-@ObjectType()
-export class MarketplaceOrder {
-  @Field(() => ID)
-  id?: string;
+// @ObjectType()
+// export class MarketplaceOrder {
+//   @Field(() => ID)
+//   id?: string;
 
-  @Field(() => Product)
-  product?: Product;
+//   @Field(() => Product)
+//   product?: Product;
 
-  @Field(() => User)
-  buyer?: User;
+//   @Field(() => User)
+//   buyer?: User;
 
-  @Field(() => User)
-  seller?: User;
+//   @Field(() => User)
+//   seller?: User;
 
-  @Field(() => Float)
-  amount?: number;
+//   @Field(() => Float)
+//   amount?: number;
 
-  @Field(() => MarketplaceOrderStatus)
-  status?: MarketplaceOrderStatus;
+//   @Field(() => MarketplaceOrderStatus)
+//   status?: MarketplaceOrderStatus;
 
-  @Field(() => EscrowStatus)
-  escrowStatus?: EscrowStatus;
+//   @Field(() => EscrowStatus)
+//   escrowStatus?: EscrowStatus;
 
-  @Field(() => String, { nullable: true })
-  paymentReference?: string | null;
+//   @Field(() => String, { nullable: true })
+//   paymentReference?: string | null;
 
-  @Field(() => Date)
-  createdAt?: Date;
+//   @Field(() => Date)
+//   createdAt?: Date;
 
-  @Field(() => Date)
-  updatedAt?: Date;
+//   @Field(() => Date)
+//   updatedAt?: Date;
 
-  @Field(() => Date, { nullable: true })
-  completedAt?: Date | null;
+//   @Field(() => Date, { nullable: true })
+//   completedAt?: Date | null;
 
-  @Field(() => Date, { nullable: true })
-  canceledAt?: Date | null;
+//   @Field(() => Date, { nullable: true })
+//   canceledAt?: Date | null;
 
-  @Field(() => Date, { nullable: true })
-  refundedAt?: Date | null;
-}
+//   @Field(() => Date, { nullable: true })
+//   refundedAt?: Date | null;
+// }
 
-@InputType()
-export class CreateMarketplaceOrderInput {
-  @Field(() => ID)
-  productId!: string;
-}
+// @InputType()
+// export class CreateMarketplaceOrderInput {
+//   @Field(() => ID)
+//   productId!: string;
+// }
 
-@InputType()
-export class UpdateMarketplaceOrderStatusInput {
-  @Field(() => ID)
-  orderId!: string;
+// @InputType()
+// export class UpdateMarketplaceOrderStatusInput {
+//   @Field(() => ID)
+//   orderId!: string;
 
-  @Field(() => MarketplaceOrderStatus)
-  status!: MarketplaceOrderStatus;
+//   @Field(() => MarketplaceOrderStatus)
+//   status!: MarketplaceOrderStatus;
 
-  @Field(() => String, { nullable: true })
-  paymentReference?: string;
-} 
+//   @Field(() => String, { nullable: true })
+//   paymentReference?: string;
+// } 
